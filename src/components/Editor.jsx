b654730d0197ee4e75972ff59f7e019901a96a9a
vikas90322 <vikasreddy90322@gmail.com>
2025-02-25 12:57:18 +0530

    hi

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -9,6 +9,7 @@ const Editor = ({ setPreview, setLoading }) => {
     const newText = e.target.value;
     setText(newText);
     setLoading(true);
+    
     try {
       const { data } = await axios.post("https://back-markdown-editor.vercel.app/convert", { markdown: newText });
       setPreview(data.html);
